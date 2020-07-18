4f64d6776481caeeaad5e482f9906a012a420842
Pham Viet Linh <phamvietlinh164@gmail.com>
2020-07-18 10:43:43 +0700

    add upload frontend

---
+++ client/build/precache-manifest.38d61a9fb7d58097b78cfc71244cd774.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4969be7b88a91fabcf8ed7bedaf8fca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61c9824cf5687be9940c",
+    "url": "/static/css/2.3505e99e.chunk.css"
+  },
+  {
+    "revision": "a61c2bfebc101d9b2ccf",
+    "url": "/static/css/main.f3520556.chunk.css"
+  },
+  {
+    "revision": "61c9824cf5687be9940c",
+    "url": "/static/js/2.e5c80dec.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e5c80dec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a61c2bfebc101d9b2ccf",
+    "url": "/static/js/main.df7c72ee.chunk.js"
+  },
+  {
+    "revision": "77687621c9bdb13d456d",
+    "url": "/static/js/runtime-main.974a140c.js"
+  }
+]);
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Res } from '@nestjs/common';
+
 
 
 @Controller()
