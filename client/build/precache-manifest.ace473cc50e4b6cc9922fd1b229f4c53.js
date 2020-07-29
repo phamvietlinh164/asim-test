e3cd2fbdce02ce8d36b2d0c04ba7ea2c1a1d0f1d
Pham Viet Linh <phamvietlinh164@gmail.com>
2020-07-29 10:22:19 +0700

    change Router to HashRouter

--- client/build/precache-manifest.ace473cc50e4b6cc9922fd1b229f4c53.js
+++ client/build/precache-manifest.ace473cc50e4b6cc9922fd1b229f4c53.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "64c0b357b225f27af6a5626cc5a3613f",
+    "revision": "f13a2c2e898fae231945e5457c5c8021",
     "url": "/index.html"
   },
   {
-    "revision": "81e828653f71a61db22a",
+    "revision": "5a4ed135112248d88a4a",
     "url": "/static/css/2.8b560e57.chunk.css"
   },
   {
-    "revision": "788ecdc5eb016261a7d0",
+    "revision": "d8546912a9f1c21940ff",
     "url": "/static/css/main.2e1519ad.chunk.css"
   },
   {
-    "revision": "81e828653f71a61db22a",
-    "url": "/static/js/2.04f2339e.chunk.js"
+    "revision": "5a4ed135112248d88a4a",
+    "url": "/static/js/2.3911ce89.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/static/js/2.04f2339e.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.3911ce89.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "788ecdc5eb016261a7d0",
-    "url": "/static/js/main.61b01ecf.chunk.js"
+    "revision": "d8546912a9f1c21940ff",
+    "url": "/static/js/main.b2c90295.chunk.js"
   },
   {
     "revision": "77687621c9bdb13d456d",
