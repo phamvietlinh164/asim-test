8d2ffc625252b1bbd9b92ecb2cf1f75c1f80faaf
Pham Viet Linh <phamvietlinh164@gmail.com>
2020-09-01 16:09:13 +0700

    fix bug url download

--- client/build/precache-manifest.3486a58fc89e06fc1cc2193480fb81f8.js
+++ client/build/precache-manifest.3486a58fc89e06fc1cc2193480fb81f8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "11c94e66d407f6d077d5d981c1f37ec5",
+    "revision": "38d76fbc9d9630a69b0caceaec285b0d",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.e37ea1d3.chunk.css"
   },
   {
-    "revision": "8f1a3199e38da38b0fc1",
+    "revision": "930bbe56d27f3ae5d1a2",
     "url": "/static/css/main.ef78ede4.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.a82ca7f0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8f1a3199e38da38b0fc1",
-    "url": "/static/js/main.43ac3257.chunk.js"
+    "revision": "930bbe56d27f3ae5d1a2",
+    "url": "/static/js/main.fb932cbb.chunk.js"
   },
   {
     "revision": "77687621c9bdb13d456d",
